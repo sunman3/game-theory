63777652efd2a4a5a59a58cb4d48dd9184404932
Sungmin An <sa3333@columbia.edu>
2016-11-24 16:12:53 -0500

    Update NonExistantException.java

--- NonExistantException.java
+++ NonExistantException.java
@@ -1,3 +1,9 @@
+/*************************************************
+// NonExistantException.java
+// By Sungmin An
+// Custom exception needed by GameTheory.java.
+*************************************************/
+
 public class NonExistantException extends Exception{
 	
 	public NonExistantException(String message){
