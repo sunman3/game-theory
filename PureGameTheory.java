d1d058d0a86ad2a907e8916aa21b6592c213bcb8
Sungmin An <sa3333@columbia.edu>
2016-11-24 16:10:16 -0500

    Update PureGameTheory.java

--- PureGameTheory.java
+++ PureGameTheory.java
@@ -1,12 +1,28 @@
+/**************************************************
+// PureGameTheory.java 
+// By Sungmin An
+// Handles pure cases for GameTheory.
+**************************************************/
+
 import java.util.Scanner;
 import java.util.LinkedList;  
 
 public class PureGameTheory{
+	/* Instance variables. */
+	private LinkedList<String> result; 
+	private int a11;
+	private int b11;
+	private int a12;
+	private int b12;
+	private int a21;
+	private int b21;
+	private int a22;
+	private int b22;
 	
 	public void play(){
 		Scanner input = new Scanner(System.in);
 
-		
+		/* Handles user input. */
 		System.out.println("Input the rows, separated by " +
 			"commas.");
 		System.out.println("Example:");
@@ -25,21 +41,22 @@ public class PureGameTheory{
 
 		pairs = line[1];			
 		elements = pairs.split(",");
-		int a12 = Integer.parseInt(elements[0]);
-		int b12 = Integer.parseInt(elements[1]);
+		a12 = Integer.parseInt(elements[0]);
+		b12 = Integer.parseInt(elements[1]);
 
 		line = row2.split("\\s+");
 		pairs = line[0];
 		elements = pairs.split(",");
-		int a21 = Integer.parseInt(elements[0]);
-		int b21 = Integer.parseInt(elements[1]);
+		a21 = Integer.parseInt(elements[0]);
+		b21 = Integer.parseInt(elements[1]);
 
 		pairs = line[1];
 		elements = pairs.split(",");
-		int a22 = Integer.parseInt(elements[0]);
-		int b22 = Integer.parseInt(elements[1]);
-
-		LinkedList<String> result = new LinkedList<>();
+		a22 = Integer.parseInt(elements[0]);
+		b22 = Integer.parseInt(elements[1]);
+		
+		/* Computes output. */ 
+		result = new LinkedList<>();
 		if((a11 == a12) && (a11 == a21) && (a11 == a22) &&
 			(b11 == b12) && (b11 == b21) && (b11 == b22)){
 			System.out.println("Nash equilibrium is all positions.");
